1c98b8fdd5b958d24d00d771ffad5d2426d6a843
IlyasAzeem <me.ismart@yahoo.com>
2019-02-09 23:43:47 +0800

    Changes done
    script updated

--- Replication_Package/Import_data_to_db/create_tables.py
+++ Replication_Package/Import_data_to_db/create_tables.py
@@ -1,10 +1,10 @@
 import mysql.connector
 
 mydb = mysql.connector.connect(
-    host = "localhost",
-    user = "root",
-    passwd = "1234",
-    database = "testdb"
+    host = "your host name",
+    user = "your user",
+    passwd = "your password",
+    database = "your db name"
 )
 
 my_cursor = mydb.cursor()
